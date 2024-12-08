7d5938f78c24cb1926b25881a384eb9871e914a6
alperen ismet esen <ismetesen5@gmail.com>
2024-12-08 22:29:57 +0300

    PROJE DOSYALARI OLUSTURDUM

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dfkshbnsdlf.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
